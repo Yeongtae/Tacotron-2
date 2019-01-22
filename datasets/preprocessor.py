5100aa1e3140429c58662dadb929e3266acc8a91
Yeongtae <hwak1234@hanmail.net>
2019-01-22 12:02:54 +0900

    index, basename bug fix

--- datasets/preprocessor.py
+++ datasets/preprocessor.py
@@ -37,7 +37,7 @@ def build_from_path(hparams, input_dirs, mel_dir, linear_dir, wav_dir, n_jobs=12
 				basename = parts[0]
 				wav_path = os.path.join(input_dir, 'wavs', '{}'.format(basename))
 				text = parts[1]
-				futures.append(executor.submit(partial(_process_utterance, mel_dir, linear_dir, wav_dir, basename, wav_path, text, hparams)))
+				futures.append(executor.submit(partial(_process_utterance, mel_dir, linear_dir, wav_dir, index, wav_path, text, hparams)))
 				index += 1
 
 	return [future.result() for future in tqdm(futures) if future.result() is not None]
